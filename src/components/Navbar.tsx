1740dcc7a22e9511aff0f6f4b9042c64646eb331
Olumorothy <iseoluwa89@gmail.com>
2023-07-22 20:22:20 +0100

    completes the navbar and styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,36 @@
 import Link from "next/link";
 import React from "react";
 import Menu from "./Menu";
+import CartIcon from "./CartIcon";
+import Image from "next/image";
 
 const Navbar = () => {
+  const user = false;
   return (
-    <div className="h-12 text-red-500 p-4 flex justify-between items-center border-b-2 border-b-red-500 uppercase">
+    <div className="h-12 text-red-500 p-4 flex justify-between items-center border-b-2 border-b-red-500 uppercase md:h-20 lg:px-20 xl:px-40">
+      {/* Left Links*/}
+      <div className="hidden md:flex gap-4 flex-1">
+        <Link href="/">HomePage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Contact</Link>
+      </div>
       {/* LOGO */}
-      <div className="text-xl">
+      <div className="text-xl md:font-bold flex-1 md:text-center">
         <Link href="/">Ofood</Link>
       </div>
       {/* MOBILE MENU */}
-      <div>
+      <div className="md:hidden">
         <Menu />
+      </div>
+      {/* Right Links*/}
+
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <div className=" md:absolute top-3 right-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+          <Image src="/phone.png" alt="" width={20} height={20} />
+          <span>123 456 78</span>
+        </div>
+        {!user ? <Link href="/">Login</Link> : <Link href="/menu">Orders</Link>}
+        <CartIcon />
       </div>
     </div>
   );
