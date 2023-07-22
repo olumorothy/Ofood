7f8e1697d1f0e09171549963e59a418203acd087
Olumorothy <iseoluwa89@gmail.com>
2023-07-22 16:09:40 +0100

    implements open and close modal for Navbar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,9 +1,18 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import React, { useState } from "react";
+import CartIcon from "./CartIcon";
 
+const links = [
+  { id: 1, title: "Homepage", url: "/" },
+  { id: 2, title: "Menu", url: "/menu" },
+  { id: 3, title: "Working Hours", url: "/" },
+  { id: 4, title: "Contact", url: "/" },
+];
 const Menu = () => {
   const [open, setIsOpen] = useState(false);
+  const user = false;
   return (
     <div>
       {!open ? (
@@ -22,6 +31,31 @@ const Menu = () => {
           height={20}
           onClick={() => setIsOpen(false)}
         />
+      )}
+      {open && (
+        <div className="bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10">
+          {links.map((item) => (
+            <Link
+              href={item.url}
+              key={item.id}
+              onClick={() => setIsOpen(false)}
+            >
+              {item.title}
+            </Link>
+          ))}
+          {!user ? (
+            <Link href="/login" onClick={() => setIsOpen(false)}>
+              Login
+            </Link>
+          ) : (
+            <Link href="/orders" onClick={() => setIsOpen(false)}>
+              Orders
+            </Link>
+          )}
+          <Link href="/cart" onClick={() => setIsOpen(false)}>
+            <CartIcon />
+          </Link>
+        </div>
       )}
     </div>
   );
